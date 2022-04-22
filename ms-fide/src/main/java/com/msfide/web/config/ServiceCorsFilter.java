085abca0f800d539e28dd5a290074de73a56fadc
fidelin17 <mcfidelin@gmail.com>
2022-04-21 23:19:24 -0500

    cors

--- ms-fide/src/main/java/com/msfide/web/config/ServiceCorsFilter.java
+++ ms-fide/src/main/java/com/msfide/web/config/ServiceCorsFilter.java
@@ -15,7 +15,8 @@ import java.io.IOException;
 public class ServiceCorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
+            throws IOException
+            , ServletException {
 
 
         HttpServletRequest request = (HttpServletRequest) req;
